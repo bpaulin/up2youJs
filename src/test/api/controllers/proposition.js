5b5066000f21adba358ea15f3388e2ba933b9b81
Bruno Paulin <bruno_paulin@carrefour.com>
2017-04-16 15:59:54 +0200

    routes proposer

---
+++ src/test/api/controllers/proposition.js
@@ -0,0 +1,15 @@
+var should = require('should');
+var request = require('supertest');
+var server = require('../../../app');
+
+describe('proposition', function () {
+
+  describe('GET /proposition', function () {
+
+    xit('should work', function (done) {
+      done();
+    });
+
+  });
+
+});
